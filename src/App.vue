b3cd427ea8bf1cd3f050bdd3b96e08fa900b8394
pennyswh <273472805@qq.com>
2019-02-12 00:06:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <product-list-one ></product-list-one>
+    <product-list-two ></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+export default {
+  name: 'app',
+  components:{
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  data () {
+    return {
+      products:[
+        {name:"马云",price:200},
+        {name:"马化腾",price:140},
+        {name:"马冬梅",price:100},
+        {name:"马蓉",price:60}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="product-list-one">
+    <h2>product-list-one</h2>
+    <ul>
+        <li v-for="product in saleProducts">
+            <span class="name">
+                {{product.name}}
+            </span>
+            <span class="price">
+                ${{product.price}}
+            </span>
+        </li>
+    </ul>
+    <button @click="reducePrice">商品降价</button>
+  </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        products(){
+            return this.$store.state.products;
+        },
+        saleProducts(){
+            return this.$store.getters.saleProducts;
+            // var saleProducts = this.$store.state.products.map(
+            //     product =>{
+            //         return {
+            //             name:"**"+product.name+"**",
+            //             price:product.price/2
+            //         }
+            //     }
+            // )
+            // return saleProducts;
+        }
+    },
+    methods:{
+        reducePrice:function(amount){
+            // this.$store.state.products.forEach(product =>{
+            //     product.price -=1;
+            // })
+            //  this.$store.commit('reducePrice');
+            this.$store.dispatch("reducePrice",amount);
+        }
+    }
+}
+</script>
+
+<style scoped>
+#product-list-one{
+    background: #fff8b1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+    list-style-type:none;
+}
+#product-list-one li{
+    margin-right:10px;
+    margin-top:10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight:bold;
+    color: #E8800c;
+    display: block;
+}
+</style>
